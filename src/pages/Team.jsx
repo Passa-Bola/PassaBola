be0a595fe019d813d531f7fb48189011924f9549
leeosilvp <leo965679@gmail.com>
2025-09-13 15:39:04 -0300

    18 u- atualizando tamanho de fonts

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -1,3 +1,4 @@
+import Ad from "../components/Ad"
 import Header from "../components/Header"
 import NavBar from "../components/NavBAr"
 
@@ -5,6 +6,9 @@ function Team() {
   return (
     <section className='ctn-team'>
       <Header />
+      <div className="content">
+        <Ad />
+      </div>
       <NavBar />
     </section>
   )
