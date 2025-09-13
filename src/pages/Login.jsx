dbec5a9cd38c1739a60aa4179a7469bfff68b3ec
leeosilvp <leo965679@gmail.com>
2025-09-13 15:51:46 -0300

    19 u- arrumando tamanho da font tela de login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,15 @@
 import Authentication from '../components/Authentication'
+import logo from '../assets/img/icon.png';
 import '../css/style.css'
 
 function Login() {
   return (
     <section className="ctn-login">
         <article className='welcome'>
+            <div className="logo">
+                <img src={logo} />
+                <h3>PassaBola</h3>
+            </div>
             <h1><span>Seja</span>Bem Vinda!</h1>
             <h2>Sua paixão pelo futebol feminino merece <span>destaque.</span></h2>
         </article>
