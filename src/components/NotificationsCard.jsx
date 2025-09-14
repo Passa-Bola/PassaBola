36b71fd73a821345ab75bcb12e1b3fdc61857d68
leeosilvp <leo965679@gmail.com>
2025-09-14 18:08:05 -0300

    25 a- criando notificação de boas-vindas

---
+++ src/components/NotificationsCard.jsx
@@ -0,0 +1,17 @@
+import '../css/notifications.css'
+
+function NotificationsCard() {
+  return (
+    <section className='feed-notifications'>
+        <article className='notification'>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8B5pQmu3WV6NFowHTtz5h9dKaCCBVJoRAeA&s" />
+            <div className="info-notification">
+                <h1>PassaBola</h1>
+                <h2>Seja bem-vinda à maior comunidade de futebol feminino! Aproveite e divirta-se com a gente</h2>
+            </div>
+        </article>
+    </section>
+  )
+}
+
+export default NotificationsCard
--- src/pages/Notifications.jsx
+++ src/pages/Notifications.jsx
@@ -1,10 +1,14 @@
 import Header from "../components/Header"
 import NavBar from "../components/NavBAr"
+import NotificationsCard from "../components/NotificationsCard"
 
 function Notifications()  {
   return (
     <section className='ctn-notifications'>
       <Header />
+      <div className="content">
+        <NotificationsCard />
+      </div>
       <NavBar />
     </section>
   )
